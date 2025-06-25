1bde9ebadab50ecc166d1a0ff7b17af9f2b8c5e4
redgtxt <hp052635@gmail.com>
2025-06-25 09:59:07 +0100

    improved the bar

--- src/Mlx/mouse_hooks.c
+++ src/Mlx/mouse_hooks.c
@@ -89,6 +89,47 @@ int mouse_move_handler(int x, int y, void *param)
     return (0);
 }
 
+static int is_mouse_on_slider_bar(t_slider slider, int mouse_x, int mouse_y)
+{
+    t_interval x_interval;
+    t_interval y_interval;
+    
+    x_interval = interval_create(slider.x, slider.x + slider.width);
+    y_interval = interval_create(slider.y, slider.y + slider.height);
+    
+    return (interval_contains(mouse_x, x_interval) && 
+            interval_contains(mouse_y, y_interval));
+}
+
+static void set_slider_value_from_position(t_slider *slider, int mouse_x)
+{
+    float ratio;
+    
+    ratio = (float)(mouse_x - slider->x) / slider->width;
+    
+    // Limitar entre 0 e 1
+    if (ratio < 0.0) ratio = 0.0;
+    if (ratio > 1.0) ratio = 1.0;
+    
+    // Calcular o novo valor
+    slider->current_value = slider->min_value + ratio * (slider->max_value - slider->min_value);
+}
+
+// Função para redesenhar toda a interface
+static void redraw_interface(t_control_panel *cp)
+{
+    clear_image_slider(cp);
+    
+    draw_button(cp, cp->config_win->button);
+    draw_slider(cp, cp->config_win->slider);
+    
+    mlx_put_image_to_window(cp->config_win->mlx, cp->config_win->win,
+                           cp->config_win->img, 0, 0);
+    
+    // Redesenhar textos
+    mlx_string_put(cp->config_win->mlx, cp->config_win->win, 175, 130, 0xFF0000, "RENDER");
+    draw_slider_values(cp, cp->config_win->slider);
+}
 int mouse_press_handler(int button, int x, int y, void *param)
 {
     t_control_panel *cp = (t_control_panel *)param;
@@ -114,15 +155,33 @@ int mouse_press_handler(int button, int x, int y, void *param)
             clear_image(cp);
             cp->amb_light.light_force = slider->current_value;
             render_scene(cp);
+            return (0);
         }
         
-        // Verificar click no slider
-        if (is_mouse_on_slider_handle(*slider, x, y))
+        // Verificar se clicou na barra do slider
+        if (is_mouse_on_slider_bar(*slider, x, y))
         {
-            slider->is_dragging = 1;
-            printf("Slider sendo arrastado!\n");
+            // Se clicou no handle, apenas inicia o drag
+            if (is_mouse_on_slider_handle(*slider, x, y))
+            {
+                slider->is_dragging = 1;
+                printf("Slider sendo arrastado!\n");
+            }
+            else
+            {
+                // Se clicou na barra (mas não no handle), pula para a posição
+                set_slider_value_from_position(slider, x);
+                slider->is_dragging = 1; // Permite continuar arrastando
+                
+                // Redesenhar imediatamente
+                redraw_interface(cp);
+                
+                printf("Slider saltou para posição! Valor: %.2f\n", slider->current_value);
+            }
         }
     }
     
     return (0);
 }
+
+//            cp->amb_light.light_force = slider->current_value;
